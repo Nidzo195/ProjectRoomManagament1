bd1d4854703acaa166c110036fe3fd84491e18ae
matovicn@bzz.ch <matovicn@bzz.ch>
2020-05-29 10:47:32 +0200

    init

---
+++ ProjektRoomManagament/View/RoomController.js
@@ -0,0 +1,55 @@
+﻿/**
+ * view-controller for raume.html
+ *
+ * M426: Room Management
+ *
+ * @author  Nina Hagenbucher
+ * @since   2020-05-15
+ * @version 1.0
+ */
+
+
+
+$(document).ready(function () {
+    showMessage("empty", " ");
+
+    /**
+     * when opened --> sends data form server to view
+     */
+    $("#loadRooms");
+
+
+});
+
+/**
+ * loads room data from server
+ * @param form the form with the data
+ */
+function loadRooms() {
+    $
+        .ajax({
+            url: "https://localhost:44369/api/Room/ListAll",
+            dataType: "json",
+            type: "GET"
+        })
+        .done(showRooms)
+        .fail(function (xhr, status, errorThrown) {
+            $("#message").text("Fehler beim Lesen der Räume.    Response Text: "+ xhr.responseText +"  Status: "+status);
+        })
+}
+
+function showRooms(roomData) {
+    $("#message").val("");
+    $("#rooms > tbody").html("");
+    var tableData = "";
+    $.each(roomData, function (id, room) {
+        tableData += "<tr>";
+        tableData += "<td>" + room.raumName + "</td>";
+        tableData += "<td>" + room.raumBeschreibung + "</td>";
+        tableData += "<td>" + room.anzahlPlaetze + "</td>";
+        tableData += "<td>" + room.kosten + "</td>";
+        tableData += "<td>" + room.ausstattung + "</td>";
+        tableData += "</tr>";
+    });
+    $("#rooms > tbody").html(tableData);
+}
