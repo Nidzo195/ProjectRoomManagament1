000b7a1e331391e0776212d3d3078bbbed5dae4a
ninahagenbucher <46046151+ninahagenbucher@users.noreply.github.com>
2020-06-12 12:16:20 +0200

    view controller

--- ProjektRoomManagament/View/js/ReservationViewController.js
+++ ProjektRoomManagament/View/js/ReservationViewController.js
@@ -34,24 +34,27 @@ function loadRooms() {
         })
 }
 
-// Todo: IDs anpassen
+/**
+ * inserts data in html
+ * @param roomData the form with the data
+ */
 function showRooms(roomData) {
 
     if (typeof roomData !== 'undefined') {
-
+        //Todo: datum formatieren und substring richtig machen
         var tableData = "";
-        $("#reservationList").html(""); //auskommentiert lassen bis zum release
+        $("#reservationList").html("");
 
         $.each(roomData, function (id, room) {
-            tableData += "<div class='roomBox' id='rooms'>";
-            tableData += "<table>'";
-            tableData += "<tr><td><a>Startdatum:" + room.start.substring(0, 9) + "</a><a id='dateStart'></a></td></tr>";
-            tableData += "<tr><td><a>Startzeit:" + room.start.substring(10, 14) + "</a><a id='timeStart'></a></td></tr>";
-            tableData += "<tr><td><a>Enddatum:" + room.end.substring(0, 9) + "</a><a id='dateEnd'></a></td></tr>";
-            tableData += "<tr><td><a>Endzeit:" + room.end.substring(10, 14) + "</a><a id='timeEnd'></a></td></tr>";
-            tableData += "<tr><td><a>Name:" + room.raumName + "</a><a id='roomName'></a></td></tr>";
-            tableData += "<tr><td><a>Veranstalter:" + room.veranstalter + "</a><a id='organizer'></a></td></tr>";
-            tableData += "<tr><td><a>Telefonnummer:" + room.telefonnummer + ".-</a><a id='phoneNr'></a></td></tr>";
+            tableData += "<div id='reservation'>";
+            tableData += "<table class='reservationBox' id='reservation'>";
+            tableData += "<tr><td><a>" + room.raumName + "</a><a id='roomName'></a>";
+            tableData += "<a>Datum: " + room.start.substring(0, 10) +"</a><a id='dateStart'></a>";
+            tableData += "<a>   " + room.start.substring(14, 19) + "</a><a id='timeStart'>-</a>" ;
+            tableData += "<a>   " + room.end.substring(0, 10) + "</a><a id='dateEnd'></a>    ";
+            tableData += "<a>   " + room.end.substring(14, 19) + "</a><a id='timeEnd'></a></td></tr>";
+            tableData += "<tr><td><a>Veranstalter: " + room.veranstalter + "</a><a id='organizer'></a>";
+            tableData += "<a>" + room.telefonnummer + "</a><a id='phoneNr'></a></td></tr>";
             tableData += "</table>";
             tableData += "</div>";
         });
